ce83ac55ce6b34612daad41074d805507f749e51
Minku Singh <mailminkusingh@gmail.com>
2022-01-13 20:33:44 +0530

    project- area

---
+++ area/area.js
@@ -0,0 +1,17 @@
+let base = document.querySelector("#base");
+let height = document.querySelector("#height");
+let findAreaBtn = document.querySelector("#find-btn");
+let output = document.querySelector("#output");
+output.style.display = "none";
+
+findAreaBtn.addEventListener("click", handleBtnClick);
+
+function handleBtnClick(){
+    let area = findArea(base.value, height.value);
+    output.style.display = "block";
+    output.innerText = `Area of the triangle : ${area}`
+}
+
+function findArea(b, h){
+    return ((1/2)*b*h);
+}
